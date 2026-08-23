897412b019b039f143180cea8ef0548cb355898d
Ruei-Jr Wu <rueijrwu@gmail.com>
2026-08-23 19:21:08 -0400

    Export ranking replay workflow

--- worker/main.js
+++ worker/main.js
@@ -1,5 +1,6 @@
 import worker from "./index.js";
 import { IncrementalFeatureWorkflow } from "./incremental-workflow.js";
+import { RankingReplayWorkflow } from "./ranking-replay-workflow.js";
 
-export { IncrementalFeatureWorkflow };
+export { IncrementalFeatureWorkflow, RankingReplayWorkflow };
 export default worker;
