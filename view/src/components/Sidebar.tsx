bc9397fd1fec6c53556641a23b11c3bf7ed15815
Ruei-Jr Wu <rueijrwu@gmail.com>
2026-08-24 11:53:06 -0400

    Add backtest audit navigation

--- view/src/components/Sidebar.tsx
+++ view/src/components/Sidebar.tsx
@@ -9,6 +9,7 @@ const Sidebar: React.FC = () => {
         <li><NavLink to="/" end>Home</NavLink></li>
         <li><NavLink to="/portfolio">Portfolio</NavLink></li>
         <li><NavLink to="/watchlist">Watchlist</NavLink></li>
+        <li><NavLink to="/backtest">Backtest</NavLink></li>
         <li><NavLink to="/settings">Settings</NavLink></li>
       </ul>
     </nav>
