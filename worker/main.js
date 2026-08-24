c4cfbc92d1956bf77ccf8d43918e10e9f28f36ce
Ruei-Jr Wu <rueijrwu@gmail.com>
2026-08-23 21:04:08 -0400

    Route protected manual universe refresh through Worker entry

--- worker/main.js
+++ worker/main.js
@@ -2,6 +2,7 @@ import worker from "./index.js";
 import { BacktestWorkflow } from "./backtest-workflow.js";
 import { FeatureBootstrapWorkflow } from "./feature-bootstrap-workflow.js";
 import { IncrementalFeatureWorkflow } from "./incremental-workflow.js";
+import { maybeHandleManualAdmin } from "./manual-admin.js";
 import { RankingReplayWorkflow } from "./ranking-replay-workflow.js";
 
 export {
@@ -10,4 +11,15 @@ export {
   IncrementalFeatureWorkflow,
   RankingReplayWorkflow,
 };
-export default worker;
+
+export default {
+  async fetch(request, env, ctx) {
+    const manual = await maybeHandleManualAdmin(request, env);
+    if (manual) return manual;
+    return worker.fetch(request, env, ctx);
+  },
+
+  async scheduled(controller, env, ctx) {
+    return worker.scheduled(controller, env, ctx);
+  },
+};
