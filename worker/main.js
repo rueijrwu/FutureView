dcc2c616e1db9020d6b88da399a2f0e7b6f1c8c6
Ruei-Jr Wu <rueijrwu@gmail.com>
2026-08-23 20:34:38 -0400

    Export JS backtest Workflow

--- worker/main.js
+++ worker/main.js
@@ -1,6 +1,11 @@
 import worker from "./index.js";
+import { BacktestWorkflow } from "./backtest-workflow.js";
 import { IncrementalFeatureWorkflow } from "./incremental-workflow.js";
 import { RankingReplayWorkflow } from "./ranking-replay-workflow.js";
 
-export { IncrementalFeatureWorkflow, RankingReplayWorkflow };
+export {
+  BacktestWorkflow,
+  IncrementalFeatureWorkflow,
+  RankingReplayWorkflow,
+};
 export default worker;
