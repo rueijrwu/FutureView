ebacbfa97508911b1e2f1a7f383099b588b7b077
Ruei-Jr Wu <rueijrwu@gmail.com>
2026-08-24 11:52:43 -0400

    Route backtest audit page

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -4,6 +4,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
 import Watchlist from './pages/Watchlist';
+import Backtest from './pages/Backtest';
 import Settings from './pages/Settings';
 import './App.css';
 
@@ -15,6 +16,7 @@ const App: React.FC = () => {
           <Route index element={<Home />} />
           <Route path="portfolio" element={<Portfolio />} />
           <Route path="watchlist" element={<Watchlist />} />
+          <Route path="backtest" element={<Backtest />} />
           <Route path="settings" element={<Settings />} />
         </Route>
       </Routes>
