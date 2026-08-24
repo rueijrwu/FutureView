3165b1cc6c14b5712814096ed164a2fd9270e403
Ruei-Jr Wu <rueijrwu@gmail.com>
2026-08-23 20:36:59 -0400

    Export JS feature bootstrap Workflow

--- worker/main.js
+++ worker/main.js
@@ -1,10 +1,12 @@
 import worker from "./index.js";
 import { BacktestWorkflow } from "./backtest-workflow.js";
+import { FeatureBootstrapWorkflow } from "./feature-bootstrap-workflow.js";
 import { IncrementalFeatureWorkflow } from "./incremental-workflow.js";
 import { RankingReplayWorkflow } from "./ranking-replay-workflow.js";
 
 export {
   BacktestWorkflow,
+  FeatureBootstrapWorkflow,
   IncrementalFeatureWorkflow,
   RankingReplayWorkflow,
 };
