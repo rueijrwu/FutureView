ee1186a2626bf95982698bccfa4a1ffbe251ceae
Ruei-Jr Wu <rueijrwu@gmail.com>
2026-08-23 22:03:29 -0400

    Register one-time state adoption workflow

--- worker/main.js
+++ worker/main.js
@@ -4,12 +4,14 @@ import { FeatureBootstrapWorkflow } from "./feature-bootstrap-workflow.js";
 import { IncrementalFeatureWorkflow } from "./incremental-workflow.js";
 import { maybeHandleManualAdmin } from "./manual-admin.js";
 import { RankingReplayWorkflow } from "./ranking-replay-workflow.js";
+import { StateAdoptionWorkflow } from "./state-adoption-workflow.js";
 
 export {
   BacktestWorkflow,
   FeatureBootstrapWorkflow,
   IncrementalFeatureWorkflow,
   RankingReplayWorkflow,
+  StateAdoptionWorkflow,
 };
 
 export default {
