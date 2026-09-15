efb6050bb98183a8872af04e867ece12ce730b1a
Ruei-Jr Wu <rueijrwu@gmail.com>
2026-09-15 17:46:00 -0400

    fix(auth): cap PBKDF2 iterations for Workers runtime

--- cloudflare/worker/auth.js
+++ cloudflare/worker/auth.js
@@ -1,6 +1,6 @@
 const SESSION_COOKIE = "fv_session";
 const SESSION_TTL_SECONDS = 60 * 60 * 24 * 30;
-const PBKDF2_ITERATIONS = 210000;
+const PBKDF2_ITERATIONS = 100000;
 const encoder = new TextEncoder();
 
 function base64url(bytes) {
